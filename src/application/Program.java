50d6eb7aa7d1221395f30bbb5f44ccbefa333bbb
Pereira2013 <robsonwppereira2013@gmail.com>
2022-03-02 11:32:59 -0300

    inserido conceito de Encapsulameto

--- src/application/Program.java
+++ src/application/Program.java
@@ -19,8 +19,14 @@ public class Program {
     double price = sc.nextDouble();
    /* System.out.print("Quantity in stock: ");
     int quantity = sc.nextInt();*/
-
     Product product = new Product(name, price);
+
+    //caso quera alterar ou atualizar o name ou o price.
+
+    product.setName("Computer");
+    System.out.println("Updated name: " + product.getName());
+    product.setPrice(1200.00);
+    System.out.println("Updated price: " + product.getPrice());
 
     System.out.println();
     System.out.println("Product data: " + product);
--- src/entities/Product.java
+++ src/entities/Product.java
@@ -1,9 +1,9 @@
 package entities;
 
 public class Product {
-  public String name;
-  public double price;
-  public int quantity;
+  private String name;
+  private double price;
+  private int quantity;
 
   //construtor padrão
 
@@ -21,6 +21,26 @@ public class Product {
   public Product(String name, double price) {
     this.name = name;
     this.price = price;
+  }
+
+  //Encapsulamento
+
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+  public void setPrice(double price) {
+    this.price = price;
+  }
+// pelo regra de negocio o quantity só pode ser alterada pelo meio das operações criado no metodo.
+  public int getQuantity() {
+    return quantity;
   }
 
   public double totalValueStock() {
